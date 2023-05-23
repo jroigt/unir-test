a997f1d5994df01e5017bbdc1ac2a316d4f8a52e
jroigt <joseproigtorres@gmail.com>
2023-05-23 13:26:00 +0200

    feat: Added tests

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -2,6 +2,7 @@ import http.client
 import os
 import unittest
 from urllib.request import urlopen
+from urllib.error import HTTPError
 
 import pytest
 
@@ -21,3 +22,56 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
+
+    def test_api_substract(self):
+        url = f"{BASE_URL}/calc/substract/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+    def test_api_power(self):
+        url = f"{BASE_URL}/calc/power/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+    def test_api_multiply(self):
+        url = f"{BASE_URL}/calc/multiply/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+    def test_api_divide(self):
+        url = f"{BASE_URL}/calc/divide/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+    def test_api_squareroot(self):
+        url = f"{BASE_URL}/calc/squareroot/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+    def test_api_log10(self):
+        url = f"{BASE_URL}/calc/log10/5"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+
+    def test_api_divide_fail(self):
+        url = f"{BASE_URL}/calc/divide/1/0"
+        self.assertRaises(HTTPError, urlopen, url, timeout=DEFAULT_TIMEOUT)
+    def test_api_log10_fail(self):
+        url = f"{BASE_URL}/calc/log10/0"
+        self.assertRaises(HTTPError, urlopen, url, timeout=DEFAULT_TIMEOUT)
+
+    def test_api_squareroot_fail(self):
+        url = f"{BASE_URL}/calc/squareroot/%2D5"
+        self.assertRaises(HTTPError, urlopen, url, timeout=DEFAULT_TIMEOUT)
--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -24,6 +24,33 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(1, self.calc.divide(2, 2))
         self.assertEqual(1.5, self.calc.divide(3, 2))
 
+    def test_substract_method_returns_correct_result(self):
+        self.assertEqual(0, self.calc.substract(2, 2))
+        self.assertEqual(1, self.calc.substract(3, 2))
+        self.assertEqual(0.5, self.calc.substract(1, 0.5))
+        self.assertEqual(1.5, self.calc.substract(1, -0.5))
+
+    def test_power_method_returns_correct_result(self):
+        self.assertEqual(4, self.calc.power(2, 2))
+        self.assertEqual(9, self.calc.power(3, 2))
+        self.assertEqual(2, self.calc.power(4, 0.5))
+
+    @patch('app.util.validate_permissions', side_effect=mocked_validation, create=True)
+    def test_multiply_method_returns_correct_result(self, _validate_permissions):
+        self.assertEqual(4, self.calc.multiply(2, 2))
+        self.assertEqual(0, self.calc.multiply(1, 0))
+        self.assertEqual(0, self.calc.multiply(-1, 0))
+        self.assertEqual(-2, self.calc.multiply(-1, 2))
+
+    def test_squareroot_method_returns_correct_result(self):
+        self.assertEqual(2, self.calc.squareroot(4))
+        self.assertEqual(4, self.calc.squareroot(16))
+        self.assertEqual(0.7071067811865476, self.calc.squareroot(0.5))
+
+    def test_log10_method_returns_correct_result(self):
+        self.assertEqual(-0.3010299956639812, self.calc.log10(0.5))
+        self.assertEqual(1.9542425094393248, self.calc.log10(90))
+
     def test_add_method_fails_with_nan_parameter(self):
         self.assertRaises(TypeError, self.calc.add, "2", 2)
         self.assertRaises(TypeError, self.calc.add, 2, "2")
@@ -38,18 +65,42 @@ class TestCalculate(unittest.TestCase):
         self.assertRaises(TypeError, self.calc.divide, 2, "2")
         self.assertRaises(TypeError, self.calc.divide, "2", "2")
 
+    def test_multiply_method_fails_with_nan_parameter(self):
+        self.assertRaises(TypeError, self.calc.multiply, "2", 2)
+        self.assertRaises(TypeError, self.calc.multiply, 2, "2")
+        self.assertRaises(TypeError, self.calc.multiply, "2", None)
+
+    def test_power_method_fails_with_nan_parameter(self):
+        self.assertRaises(TypeError, self.calc.power, "2", 2)
+        self.assertRaises(TypeError, self.calc.power, 2, "2")
+        self.assertRaises(TypeError, self.calc.power, "2", None)
+
+    def test_squareroot_method_fails_with_nan_parameter(self):
+        self.assertRaises(TypeError, self.calc.squareroot, "2")
+        self.assertRaises(TypeError, self.calc.squareroot, None)
+        self.assertRaises(TypeError, self.calc.squareroot, object())
+
+    def test_log10_method_fails_with_nan_parameter(self):
+        self.assertRaises(TypeError, self.calc.log10, "2")
+        self.assertRaises(TypeError, self.calc.log10, None)
+        self.assertRaises(TypeError, self.calc.log10, object())
+
     def test_divide_method_fails_with_division_by_zero(self):
         self.assertRaises(TypeError, self.calc.divide, 2, 0)
         self.assertRaises(TypeError, self.calc.divide, 2, -0)
         self.assertRaises(TypeError, self.calc.divide, 0, 0)
         self.assertRaises(TypeError, self.calc.divide, "0", 0)
 
-    @patch('app.util.validate_permissions', side_effect=mocked_validation, create=True)
-    def test_multiply_method_returns_correct_result(self, _validate_permissions):
-        self.assertEqual(4, self.calc.multiply(2, 2))
-        self.assertEqual(0, self.calc.multiply(1, 0))
-        self.assertEqual(0, self.calc.multiply(-1, 0))
-        self.assertEqual(-2, self.calc.multiply(-1, 2))
+    def test_squareroot_method_fails_with_negative_number(self):
+        self.assertRaises(TypeError, self.calc.squareroot, -2)
+        self.assertRaises(TypeError, self.calc.squareroot, -0.5)
+
+    def test_log10_method_fails_with_negative_number(self):
+        self.assertRaises(TypeError, self.calc.log10, 0)
+        self.assertRaises(TypeError, self.calc.log10, -0.5)
+        self.assertRaises(TypeError, self.calc.log10, -10)
+
+
 
 
 if __name__ == "__main__":  # pragma: no cover
